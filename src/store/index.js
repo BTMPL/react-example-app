51b732309c9396e515fea65b3d1bb9682233f3a1
Bartosz Szczecinski <bartosz.szczecinski@gft.com>
2016-10-17 09:38:33 +0200

    added redux-devtools-extension support

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,11 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk"
 
 import videoReducer from "../reducers/videos";
 
 export default createStore(combineReducers({
   videos: videoReducer
-}), applyMiddleware(thunk));
+}), compose(
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
